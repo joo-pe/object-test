abdf7f8dba9174c3c6cb8e6ed70f4ff72db4eb62
joo-pe <joohyung1002@naver.com>
2023-06-04 14:35:59 +0900

    complie file delete

--- src/cleancode/Test3.java
+++ src/cleancode/Test3.java
@@ -1,5 +1,27 @@
 package cleancode;
 
-public class Test3 {
+import java.util.List;
 
+/**
+ * 일급컬렉션 써보기
+ */
+public class Test3 {
+    public class RacingGame {
+        private final List<Car> cars;
+        String carName;
+        //...
+
+        public RacingGame(String carInputs) {
+            cars = initCars(carInputs.trim());
+        }
+        // ...
+
+        public List<Car> initCars(String carName) {
+            return null;
+        }
+    }
+
+    public class Car() {
+
+    }
 }
